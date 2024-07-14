c488b4f17d5d8ad4026d4fbc13c17df5918018a7
arnonr <arnonr@kmutnb.ac.th>
2024-07-14 15:46:24 +0700

    callback

--- src/views/thaid/callback.vue
+++ src/views/thaid/callback.vue
@@ -2,34 +2,53 @@
   <div>Loading...</div>
 </template>
 
-<script setup>
-import { onMounted } from "vue";
-import axios from "axios";
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  reactive,
+  onMounted,
+  onUnmounted,
+  toRefs,
+} from "vue";
+import ApiService from "@/core/services/ApiService";
 import { useRouter } from "vue-router";
 
-const router = useRouter();
+export default defineComponent({
+  name: "callback",
+  components: {},
+  setup() {
+    // UI
+    const router = useRouter();
 
-onMounted(async () => {
-  const code = new URLSearchParams(window.location.search).get("code");
-  if (code) {
-    try {
-      const response = await axios.post(
-        "https://thaid.example.com/oauth2/token",
-        {
-          client_id: "YOUR_CLIENT_ID",
-          client_secret: "YOUR_CLIENT_SECRET",
-          code: code,
-          redirect_uri: "YOUR_REDIRECT_URI",
-          grant_type: "authorization_code",
+    // Mounted
+    onMounted(async () => {
+      const code = new URLSearchParams(window.location.search).get("code");
+      if (code) {
+        try {
+          const { data } = await ApiService.post(
+            "https://thaid.example.com/oauth2/token",
+            {
+              client_id: "YOUR_CLIENT_ID",
+              client_secret: "YOUR_CLIENT_SECRET",
+              code: code,
+              redirect_uri: "YOUR_REDIRECT_URI",
+              grant_type: "authorization_code",
+            }
+          );
+          const token = data.data.access_token;
+          console.log("Access Token:", token);
+          router.push(import.meta.env.VITE_APP_BASE_URL + "/appeal?type_id=1");
+        } catch (error) {
+          console.error("Error fetching access token:", error);
         }
-      );
-      const token = response.data.access_token;
-      // ใช้ token สำหรับเรียกข้อมูลผู้ใช้ หรือจัดการ authentication
-      console.log("Access Token:", token);
-      router.push("/"); // เปลี่ยนเส้นทางหลังจากยืนยันตัวตนสำเร็จ
-    } catch (error) {
-      console.error("Error fetching access token:", error);
-    }
-  }
+      }
+    });
+
+    onUnmounted(() => {});
+
+    // Return
+    return {};
+  },
 });
 </script>
